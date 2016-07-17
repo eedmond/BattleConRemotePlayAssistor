21028b3c2539c044c9f94ed1e1f65c84812f7402
Eric Edmond <eedmond@umich.edu>
2016-07-16 18:56:33 -0700

    Create user control for tiles of game board.

---
+++ BattleConAssistor/GameTile.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace BattleConAssistor
+{
+    public sealed partial class GameTile : UserControl
+    {
+        public GameTile()
+        {
+            this.InitializeComponent();
+            Tile.DataContext = this;
+        }
+
+        public Brush Fill
+        {
+            get { return (Brush)GetValue(FillProperty); }
+            set { SetValue(FillProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Fill.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FillProperty =
+            DependencyProperty.Register("Fill", typeof(Brush), typeof(GameTile), new PropertyMetadata(new SolidColorBrush(Colors.White)));
+    }
+}
